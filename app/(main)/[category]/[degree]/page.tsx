a28a14593ccec4599503bd17fb95b0832c522be5
Inframecollege <Inframecollege@gmail.com>
2025-03-03 17:17:51 +0530

    bug fixed

--- app/(main)/[category]/[degree]/page.tsx
+++ app/(main)/[category]/[degree]/page.tsx
@@ -1,47 +1,40 @@
-import { courseTypes } from "../../../../utils/courseTypes"
-
-import { notFound } from "next/navigation"
-import CoursePage from "../../../../components/Courses/CoursePage"
-import { use } from 'react'
+import { courseTypes } from "../../../../utils/courseTypes";
+import { notFound } from "next/navigation";
+import CoursePage from "../../../../components/Courses/CoursePage";
 
 type ParamsType = { 
-  category: string, 
-  degree: string 
-}
+  category: string; 
+  degree: string; 
+};
 
-export default function DegreePage(props: { params: Promise<ParamsType> }) {
-  const { category, degree } = use(props.params)
-  const categoryLower = category.toLowerCase()
+export default function DegreePage({ params }: { params: ParamsType }) {
+  const { category, degree } = params; // No need to use 'use' or treat it as a Promise
+  const categoryLower = category.toLowerCase();
 
   if (!courseTypes[categoryLower]) {
-    return notFound()
+    return notFound();
   }
 
-  const categoryCourses = courseTypes[categoryLower]
-  const selectedCourseIndex = categoryCourses.findIndex((course) => course.value === degree)
+  const categoryCourses = courseTypes[categoryLower];
+  const selectedCourseIndex = categoryCourses.findIndex((course) => course.value === degree);
 
   if (selectedCourseIndex === -1) {
-    return notFound()
+    return notFound();
   }
 
-  const initialTabIndex = selectedCourseIndex
+  const initialTabIndex = selectedCourseIndex;
 
-
-
-  return (
-    <>
-     
-      <CoursePage courseType={categoryCourses} category={categoryLower} initialTabIndex={initialTabIndex} />
-    </>
-  )
+  return <CoursePage courseType={categoryCourses} category={categoryLower} initialTabIndex={initialTabIndex} />;
 }
 
-
-
-// Generate Static Paths for Dynamic Routing
 export async function generateStaticParams() {
-  return Object.keys(courseTypes).map((category) => ({
-    category,
-    degree: courseTypes[category].map((course) => course.value)
-  }))
+  const paths: { category: string; degree: string }[] = [];
+
+  Object.entries(courseTypes).forEach(([category, courses]) => {
+    courses.forEach((course) => {
+      paths.push({ category, degree: course.value });
+    });
+  });
+
+  return paths;
 }
