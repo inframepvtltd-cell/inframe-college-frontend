eb1bf324c54213ccc14b5770df7f96a3c5209e5d
Zorochan404 <inframecollege@gmail.com>
2025-03-21 16:09:29 +0530

    ...

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import { Poppins } from "next/font/google";
 import "./globals.css";
-import Script from "next/script";
+// import Script from "next/script";
 import Head from "next/head"; // Import Next.js Head component
 
 const poppins = Poppins({
