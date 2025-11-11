eb93c5dc3fdc0304a04759efabc67fb30b12b02d
Inframecollege <inframecollege@gmail.com>
2025-11-11 12:43:41 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -92,9 +92,7 @@ const nextConfig = {
     ],
     minimumCacheTTL: 60,
     dangerouslyAllowSVG: true,
-    eslint: {
-    ignoreDuringBuilds: true,
-  },
+ 
     contentSecurityPolicy:
       "default-src 'self'; script-src 'self' 'unsafe-inline' 'unsafe-eval'; img-src 'self' https: data:; media-src 'none'; frame-src 'none';",
   },
