2bc0c4e15fdb9d1d8367fcce256dece81c364913
Inframecollege <inframecollege@gmail.com>
2025-11-28 18:21:03 +0530

    Update demometa.tsx

--- components/demometa.tsx
+++ components/demometa.tsx
@@ -12,8 +12,7 @@ export default function PageClient() {
 
     return (
         <div className="bg-white text-black mt-20">
-            <CourseHero />
-            <ComboPack />
+<CourseHero price="1499" />            <ComboPack />
             <CourseFeatures />
             <Customizecourse />
             <WhyChooseUs />
