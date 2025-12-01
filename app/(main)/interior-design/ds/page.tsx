f39360f5584fa423996b6a18ace0258d39e444ea
Inframecollege <YOUR_CORRECT_EMAIL>
2025-12-01 11:24:18 +0530

    parameter fixed

--- app/(main)/interior-design/ds/page.tsx
+++ app/(main)/interior-design/ds/page.tsx
@@ -3,6 +3,7 @@
 import ComboPack from "../../../../components/courseDetails/ComboPack";
 import CourseHero from "../../../../components/courseDetails/courseDetails";
 import CourseFeatures from "../../../../components/courseDetails/courseFeature";
+import CustomizeCourse from "../../../../components/courseDetails/CustomizePack";
 import Customizecourse from "../../../../components/courseDetails/CustomizePack";
 import WhyChooseUs from "../../../../components/courseDetails/WhyChooseUs";
 import PageClient from "../../../../components/demometa";
@@ -22,6 +23,14 @@ function CourseContent() {
   //   script.async = true;
   //   document.body.appendChild(script);
   // }, []);
+          const graphicDesignSoftware = [
+    { id: "1", name: "Adobe Photoshop", price: 1999, category: "design" },
+    { id: "2", name: "Adobe Illustrator", price: 1999, category: "design" },
+    { id: "3", name: "Figma", price: 1499, category: "ui-ux" },
+    { id: "4", name: "Canva Pro", price: 999, category: "design" },
+    { id: "5", name: "Adobe InDesign", price: 1799, category: "publishing" },
+    { id: "6", name: "CorelDRAW", price: 1599, category: "design" }
+  ]
   return (
     <div className="bg-white text-black mt-20">
       <CourseHero price="1499" />
@@ -34,7 +43,10 @@ function CourseContent() {
       {/* <div className="w-44 mb-10 h-1 bg-gray-500 mx-auto rounded-full"></div> */}
       {/* < DreamsSection /> */}
 
-      <Customizecourse />
+ <CustomizeCourse
+                      courseTitle="Graphic Design"
+                      availableSoftware={graphicDesignSoftware}
+                  />      
       {/* <div className="w-44 mb-10 h-1 bg-gray-500 mx-auto rounded-full"></div> */}
       < DreamsSection />
 
