bd0f60d0310fff444ff3e56329097136082abc59
Inframecollege <YOUR_CORRECT_EMAIL>
2025-12-06 10:06:41 +0530

    bug fixed

--- components/newCoursesComponent/AutoCad-Component.tsx
+++ components/newCoursesComponent/AutoCad-Component.tsx
@@ -40,14 +40,14 @@ function AutoCadComponent() {
   ]
   return (
     <div className="bg-white text-black mt-20">
-      <CourseHero price="1499" />
+      <CourseHero title="Digital Markteting" price="1499" />
       {/* <div className="w-44 mb-10 h-1 bg-gray-500 mx-auto rounded-full"></div> */}
 
       <ComboPack />
       {/* <div className="w-44 mb-10 h-1 bg-gray-500 mx-auto rounded-full"></div> */}
 
        <CustomizeCourse
-                      courseTitle="Graphic Design"
+                      courseTitle="Digital Markteting"
                       // availableSoftware={graphicDesignSoftware}
                   />      
 
