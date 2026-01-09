547a67dfc9e59ece2829d20fc00152bdbdf57d64
saleh-expansionjs <saleh@expansionjs.com>
2026-01-09 17:39:01 +0530

    revoked utm

--- app/layout.tsx
+++ app/layout.tsx
@@ -101,20 +101,6 @@ export default async function RootLayout({
   return (
     <html lang="en" className={poppins.className}>
       <head>
-        {/* Google Tag Manager */}
-        <Script id="gtm-script" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];
-            w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
-            var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
-            j.async=true;
-            j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
-            f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5XFRQ5LP');
-          `}
-        </Script>
-        {/* End Google Tag Manager */}
         <Script
           id="meta-pixel"
           strategy="afterInteractive"
@@ -213,17 +199,6 @@ export default async function RootLayout({
       </head>
 
       <body>
-        {/* Google Tag Manager (noscript) */}
-      <noscript>
-          <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5XFRQ5LP"
-            height="0"
-            width="0"
-            style={{ display: "none", visibility: "hidden" }}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
-        
         {/* Facebook PageView on route change */}
         <FacebookPixel />
         {/* Google Analytics (placed inside <body>, not inside <Head>) */}
