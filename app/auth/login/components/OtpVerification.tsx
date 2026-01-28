08db575e4640e725745203de284071dee9f16d74
inframepvtltd-cell <inframepvtltd@gmail.com>
2026-01-28 19:20:52 +0530

    Update OtpVerification.tsx

--- app/auth/login/components/OtpVerification.tsx
+++ app/auth/login/components/OtpVerification.tsx
@@ -101,7 +101,11 @@ export default function OTPVerification({
           {otp.map((digit, index) => (
             <input
               key={index}
-              ref={(el) => (inputRefs.current[index] = el)}
+              ref={(el) => {
+  inputRefs.current[index] = el;
+}}
+
+              {/* ref={(el) => (inputRefs.current[index] = el)} */}
               type="text"
               inputMode="numeric"
               pattern="\d*"
